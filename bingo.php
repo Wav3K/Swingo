4ba32c4bf1823fad503375ec8eb1c581a23e0e50
Bartosz Wronowski <134049865+Wav3K@users.noreply.github.com>
2024-04-17 13:10:07 +0200

    Add files via upload

---
+++ bingo.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Twoje Bingo</title>
+    <link rel="stylesheet" href="styleBingo.css">
+    <script src="bingo.js" defer></script>
+    <script src="script.js" defer></script>
+
+</head>
+<body>
+    <?php 
+        function createBingoArray($size) {
+            $bingoArray = [];
+            for($i = 1; $i <= $size * $size; $i++){
+                $bingoArray[$i - 1] = $_POST['input' . $i];
+            }
+            return $bingoArray;
+        }
+
+        $isLargeBingo = isset($_POST['input15']);
+        $bingoNumbers = createBingoArray($isLargeBingo ? 5 : 3);
+        // shuffle($bingoNumbers);
+        $bingoNumbersJSON = json_encode($bingoNumbers);
+    ?>
+    <div id="dane" data-associated-tab='<?php echo htmlspecialchars($bingoNumbersJSON); ?>'></div>
+    <div class="table-wrapper">
+        <table id="table"></table>
+        <p id="resultBingo"></p></body>
+    </div>
+</html>
